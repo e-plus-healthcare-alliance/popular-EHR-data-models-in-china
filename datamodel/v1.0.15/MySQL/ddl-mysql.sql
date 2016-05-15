dd3831092e935eab36cd6d7d9e42b911cf26b6ac
wanghaisheng <edwin_uestc@163.com>
2016-05-15 23:33:52 +0800

    generate html from word doc

---
+++ datamodel/v1.0.15/MySQL/ddl-mysql.sql
@@ -0,0 +1,47 @@
+/*********************************************************************************
+# Copyright 2016-05 e-plus-healthcare-alliance
+#
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# 说明: 全部用红字标识的表及字段-----未使用状态
+
+# 全部用蓝字标识的表及字段------新增且在用状态
+
+# 用蓝字标识的表字段------新增且在用状态
+
+# 黑字标识的表字段------在用状态
+
+
+
+********************************************************************************/
+
+/************************
+ ####### ####### #     #       #         #####   ######  #     #    #    #    #
+ #     	 #     # #     #      # #       #        #     # ##   ##    #    #    #       
+ #     	 # 	   # #     #     #   #      #        #     # # # # #    #    #    #       
+ ####### ######  #######    #######     #        #     # #  #  #    #    #    # 
+ #     	 #       #     #   #       #    #        #     # #     #    #    #    #    
+ #     	 #       #     #  #         #   #        #     # #     #     #  #     #     
+ ####### #       #     # #           #   #####   ######  #     #      ##      # 
+                                                                              
+script to create junweiyihao common data model, version 5.0 for PostgreSQL database
+last revised: 1-May-2016
+Authors:  Wanghaisheng
+*************************/
+
+
+/************************
+公共部分
+************************/
+
